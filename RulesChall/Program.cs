d6dc85cab94d06ad5e637fcee0c35a5e45e8bf1d
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-09 20:23:50 +0200

    UI Code
    Created UI code to show personModel method work

--- RulesChall/Program.cs
+++ RulesChall/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using RulesChall;
+
+Console.Write("FirstName: ");
+string firstName = Console.ReadLine();
+
+Console.Write("LastName: ");
+string lastName = Console.ReadLine();
+
+Console.Write("PostalCode: ");
+string postalCode = Console.ReadLine();
+
+PersonModel person = new() { FirstName = firstName, LastName = lastName, PostalCode = postalCode };
+person.ApplyRules(PrintToConsole);
+
+void PrintToConsole(string messege)
+{
+    Console.WriteLine(messege);
+}
