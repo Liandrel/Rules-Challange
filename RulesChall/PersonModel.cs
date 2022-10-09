6f1ad751acd0d13e232d5b4ce957dbe4200abd47
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-09 20:22:58 +0200

    PersonModelClass
    Created Person Model class with applyrules method that checks for main goal questions

--- RulesChall/PersonModel.cs
+++ RulesChall/PersonModel.cs
@@ -8,6 +8,25 @@ namespace RulesChall
 {
     public class PersonModel
     {
-        
+        public delegate void UIDialog(string messege);
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PostalCode { get; set; }
+
+        public void ApplyRules(UIDialog dialog)
+        {
+            if (LastName == "Corey" && FirstName != "Tim")
+            {
+                dialog("Possibly related to Tim Corey");
+            }
+            if (FirstName[0] == 'T' && LastName[0] == 'C')
+            {
+                dialog("Same initials as Tim Corey");
+            }
+            if (PostalCode.Substring(0, 2) == "18")
+            {
+                dialog("In the same general area as Tim");
+            }
+        }
     }
 }
