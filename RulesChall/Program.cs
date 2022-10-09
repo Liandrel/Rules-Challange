e568220d7b13b11ed48c6a8b0a626bfce1c8efa9
Liandrel <109763767+Liandrel@users.noreply.github.com>
2022-10-09 21:00:51 +0200

    Logic Update
    deleted hard coded logic for rules checking

--- RulesChall/Program.cs
+++ RulesChall/Program.cs
@@ -10,9 +10,4 @@ Console.Write("PostalCode: ");
 string postalCode = Console.ReadLine();
 
 PersonModel person = new() { FirstName = firstName, LastName = lastName, PostalCode = postalCode };
-person.ApplyRules(PrintToConsole);
 
-void PrintToConsole(string messege)
-{
-    Console.WriteLine(messege);
-}
